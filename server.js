6e43dae73b9451749531b71d31aeb1fa46bcb741
Roberto Alvarez <robertoatc02@gmail.com>
2014-10-17 13:19:28 -0700

    Revert to express

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 console.log('Booting up');
 
-
 var express,
   app,
   config;
-
-config = require('./config/config');
-// require('./config/express')(app);
-// require('./handlers/request-handler.js')(app);
 
 setInterval(function() {
   console.log('Im on a node!');
 }, 3000);
 
-var server = require('http').createServer(app);
-server.listen(config.port, function(){
-  console.log('listening');
+express = require('express');
+app = express();
+
+config = require('./config/config');
+require('./config/express')(app);
+require('./handlers/request-handler.js')(app);
+
+
+app.listen(config.port, function() {
+  console.log('Express server now listening on: ' + config.port);
 });
-
-// express = require('express');
-// app = express();
-
-
-
-// app.listen(config.port, function() {
-//   console.log('Express server now listening on: ' + config.port);
-// });
